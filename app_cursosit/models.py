37f181c0bfeefecffdf455b2f9cdaf5a4656f99a
untdf-h <hectoruntdf@gmail.com>
2024-05-23 22:06:03 -0300

    Se reestructura el modelo

--- app_cursosit/models.py
+++ app_cursosit/models.py
@@ -1,41 +1,56 @@
 from django.db import models
 
-# Create your models here.
 
+class Imagen (models.Model):
+    arch_imagen = models.FileField(upload_to='imagenes/')
 
-class EstadoCurso(models.Model):
-    CONDICIONES_CHOICES = (
-        ('PAGO', 'Pago'),
-        ('GRATUITO', 'Gratuito'),
-    )
-    condicion = models.CharField(max_length=10, choices=CONDICIONES_CHOICES)
+class Video (models.Model):
+    arch_video = models.FileField(upload_to='videos/')
 
+class Pago (models.Model):
+    DEBITO = 'DE'
+    CREDITO = 'CR'
+    FORMA_PAGO_CHOICES = [
+        (DEBITO, 'Debito'),
+        (CREDITO, 'Credito'),
+    ]
+    monto = models.DecimalField(max_digits=8, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+    forma_pago = models.CharField(max_length=2, choices=FORMA_PAGO_CHOICES)
 
-class Curso(models.Model):
-    nombre = models.CharField(max_length=30)
-    descripcion = models.CharField(max_length=50)
-    carga_horaria = models.IntegerField()
-    condicion = models.ForeignKey("EstadoCurso",on_delete=models.CASCADE)
-    plan_estudio = models.CharField(max_length=100)
-    duracion = models.IntegerField() # semanas/etc.
+class Leccion (models.Model):
+    tema = models.CharField(max_length=40)
+    duracion = models.TimeField()
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
 
-class Compra(models.Model):
-    total = models.PositiveIntegerField()
+class Modulo (models.Model):
+    nombre = models.CharField(max_length=50)
+    cantidad = models.IntegerField()
+    leccion = models.ForeignKey(Leccion,on_delete=models.CASCADE)
 
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=40)
-    apellido = models.CharField(max_length=40)
-    email = models.EmailField()
-
-#class Administrador(user.User)
-
-#class ItemVendido(models.Model):
-
-
-class Imagen(models.Model):
+class Curso (models.Model):
+    GRATUITO = 'GR'
+    PAGO = 'PA'
+    CONDICION_CHOICES = [
+        (GRATUITO, 'Gratuito'),
+        (PAGO, 'Pago'),
+    ]
     nombre = models.CharField(max_length=20)
-    imagen = models.ImageField()
+    descripcion = models.CharField(max_length=40)
+    duracion = models.IntegerField()
+    cant_leccion = models.IntegerField()
+    plan_estudios = models.CharField(max_length=100)
+    condicion = models.CharField(max_length=2, choices=CONDICION_CHOICES, default=GRATUITO)
+    modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
+    imagen = models.ForeignKey(Imagen, on_delete=models.CASCADE)
 
-class Video(models.Model):
+class Alumno (models.Model):
     nombre = models.CharField(max_length=30)
-    videopath = models.FileField()
+    dni = models.BigIntegerField
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    pago = models.ForeignKey(Pago, on_delete=models.CASCADE)
+
+class Profesor (models.Model):
+    legajo = models.IntegerField()
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
