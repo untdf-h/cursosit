32be403f243f7385ee45cc97f57a40517289a176
untdf-h <hectoruntdf@gmail.com>
2024-06-04 17:26:30 -0300

    Se modifico las claves foraneas de los modelos

--- app_cursosit/models.py
+++ app_cursosit/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User,UserManager
+from django.contrib.auth.models import User,UserManager, AbstractUser
 
 
-class Imagen (models.Model):
-    arch_imagen = models.FileField(upload_to='imagenes/')
+class Alumno (models.Model):
+    alumno = models.OneToOneField(User,on_delete=models.CASCADE,blank=False,null=False)
+    nombre = models.CharField(max_length=30)
+    dni = models.PositiveBigIntegerField()
+    
 
-class Video (models.Model):
-    arch_video = models.FileField(upload_to='videos/')
+class Profesor (models.Model):
+    profesor = models.OneToOneField(User, on_delete=models.CASCADE)
+    legajo = models.IntegerField()
+
 
 class Pago (models.Model):
     DEBITO = 'DE'
@@ -18,16 +23,7 @@ class Pago (models.Model):
     monto = models.DecimalField(max_digits=8, decimal_places=2)
     fecha = models.DateTimeField(auto_now_add=True)
     forma_pago = models.CharField(max_length=2, choices=FORMA_PAGO_CHOICES)
-
-class Leccion (models.Model):
-    tema = models.CharField(max_length=40)
-    duracion = models.TimeField()
-    video = models.ForeignKey(Video, on_delete=models.CASCADE)
-
-class Modulo (models.Model):
-    nombre = models.CharField(max_length=50)
-    cantidad = models.IntegerField()
-    leccion = models.ForeignKey(Leccion,on_delete=models.CASCADE)
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
 
 
 class CursoManager(models.Manager):
@@ -42,28 +38,29 @@ class Curso (models.Model):
         (GRATUITO, 'Gratuito'),
         (PAGO, 'Pago'),
     ]
+    
     nombre = models.CharField(max_length=20)
     descripcion = models.CharField(max_length=40)
     duracion = models.IntegerField()
     cant_leccion = models.IntegerField()
     plan_estudios = models.CharField(max_length=100)
     condicion = models.CharField(max_length=2, choices=CONDICION_CHOICES, default=GRATUITO)
-    modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
-    imagen = models.ForeignKey(Imagen, on_delete=models.CASCADE)
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
 
     objects = models.Manager()
     cursos_objects = CursoManager()
+    
 
-class Alumno (User):
-    nombre = models.CharField(max_length=30)
-    dni = models.BigIntegerField
-    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-    pago = models.ForeignKey(Pago, on_delete=models.CASCADE)
+class Modulo (models.Model):
+    nombre = models.CharField(max_length=50)
+    cantidad = models.IntegerField()
+    curso = models.ForeignKey(Curso,on_delete=models.CASCADE)
 
-class Profesor (User):
-    legajo = models.IntegerField()
-    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-
+class Leccion (models.Model):
+    tema = models.CharField(max_length=40)
+    duracion = models.TimeField()
+    modulo = models.ForeignKey(Modulo,on_delete=models.CASCADE)
 
 class FavoritoManager(models.Manager):
     def get_queryset(self):
@@ -73,9 +70,12 @@ class Favorito (models.Model):
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     fecha = models.DateTimeField(auto_now_add=True)
-    
+   
     objects = models.Manager()
-    favarito_objects = FavoritoManager()
+    favoritos_objects = FavoritoManager()
+    def __str__(self):
+        return f"{self.alumno}"
+    
 
 class BusquedaManager(models.Manager):
     def get_queryset(self):
@@ -87,3 +87,11 @@ class Busqueda (models.Model):
     
     objects = models.Manager()
     busqueda_objects = BusquedaManager()
+
+class Imagen (models.Model):
+    arch_imagen = models.FileField(upload_to='imagenes/')
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+class Video (models.Model):
+    arch_video = models.FileField(upload_to='videos/')
+    video = models.ForeignKey(Leccion, on_delete=models.CASCADE)
