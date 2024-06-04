d3f8bab6be299edb2aeec822fc75fdaa14d8523a
untdf-h <hectoruntdf@gmail.com>
2024-06-04 17:33:48 -0300

    Se agrego urls en urls.py

--- cursosit/urls.py
+++ cursosit/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_cursosit.views import CursosView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', CursosView.as_view(), name="cursos"),
+
 ]
