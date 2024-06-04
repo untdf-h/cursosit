d9dca826e6ea7be345856d87b9b9fec09758def9
untdf-h <hectoruntdf@gmail.com>
2024-06-04 17:30:17 -0300

    Se cambio el nombre de la view CursosView

--- app_cursosit/views.py
+++ app_cursosit/views.py
@@ -1,3 +1,18 @@
+from django.views.generic import View
 from django.shortcuts import render
+from app_cursosit.models import *
 
-# Create your views here.
+class CursosView(View):
+    def get(self,request):
+        cursos = Curso.objects.all()
+        return render(request,'index.html',{'cursos':cursos})
+
+class FavoritosView(View):
+    def get(self,request):
+        favoritos = Favorito.objects.all()
+        return render(request,'index.html',{'favoritos':favoritos})
+    
+class BuscadosView(View):
+    def get(self,request):
+        buscados = Busqueda.objects.all()
+        return render(request, 'index.html',{'buscados':buscados})
